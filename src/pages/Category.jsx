0130a4dd0a15d4cd733dd119a5b37b412351c675
Sahandgha <sahand.gha@gmail.com>
2022-08-27 08:09:32 +1000

    fix the no current offers section

--- src/pages/Category.jsx
+++ src/pages/Category.jsx
@@ -108,7 +108,12 @@ export default function Category() {
           )}
         </>
       ) : (
-        <p>There are no current offers</p>
+        <p>
+          There are no current{" "}
+          {params.categoryName === "rent"
+            ? "places for rent"
+            : "places for sale"}
+        </p>
       )}
     </div>
   );
